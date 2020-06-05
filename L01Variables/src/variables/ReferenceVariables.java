e17374b2f4689bc9a4c730ef0a50ba9dfe930c00
Ramy-Abdulazziz <ramy.abdulazziz@gmail.com>
2020-06-04 23:10:31 -0400

    lesson one

---
+++ L01Variables/src/variables/ReferenceVariables.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class ReferenceVariables {
+
+	public static void main(String[] args) {
+
+		String name = "Edgar";
+		String referenceVariable;
+		
+		String small = "a";
+		
+		String large = "sjdfkljkjkjlll";
+		System.out.println(large.hashCode());
+		String [] strings = new String [10];
+		System.out.println(strings);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
